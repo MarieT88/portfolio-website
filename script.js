10193f7e902fc370c9066414e882cd2a8211bec3
Marie Tornetto <marietornetto@gmail.com>
2024-07-08 20:47:45 -0400

    organized folders cleaned up html

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.addEventListener('mousemove', function(e) {
     customCursor.style.left = `${e.pageX}px`;
     customCursor.style.top = `${e.pageY}px`;
 });
+
+// button style.css hot pink alternate lettering
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('styledButton');
+    const text = button.innerText;
+    const styledText = text.split('').map((letter, index) => {
+      return (index === 1 || index === 4 || index === 8) ? `<span class="hotpink-letter">${letter}</span>` : letter;
+    }).join('');
+    button.innerHTML = styledText;
+  });
+  
